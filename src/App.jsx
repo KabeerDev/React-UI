21b0f2fc9aa1d6fe3e19110f17395df131735950
Kabeer <kabirdev.777@gmail.com>
2024-05-26 16:43:57 +0500

    integrated camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import Container from './components/container'
 import Actions from './components/actions-container';
 
 function App() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+     navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: true,
+    }).then(stream=>videoRef.current.srcObject=stream).catch(err=>console.log("Error: ", err))
+  }, [])
+  
 
   return (
     <>
       <div className='main'>
         <button className='back'><img src="./img/back.svg" alt="back icon" width={17} />Back</button>
         <div className='imagePreview'>
-          <img src="./img/dummy.jpg" alt="dummy image" />
+          <video className='userVid' ref={videoRef} src="" autoPlay type="video/mp4"></video>
         </div>
         <Container />
         <Actions />
